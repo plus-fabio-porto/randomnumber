ddab98b357a02bc7b57802dc14dd17a5fd530ecc
Fabio Porto <fportodelima@gmail.com>
2024-08-09 11:13:56 -0300

    randomnumbers.bat

--- números aleatórios.bat
+++ números aleatórios.bat
@@ -1,10 +1,10 @@
 rem^ & @echo off & chcp 65001 >nul & cls & title Number by Fabio
+rem^ & setlocal enabledelayedexpansion & set count=1
 :'loop
-rem^ & CSCRIPT //nologo //e:vbscript "%~f0" & echo/
+rem^ & for /f "tokens=* USEBACKQ" %%f in (`CSCRIPT //nologo //e:vbscript "%~f0"`) do (set "var=%%f")
+rem^ & echo %count%º number: %var% & set /a count=!count!+1 & echo/
 rem^ & pause >nul
 rem^ & goto :'loop
 
-Max=10
-
 randomize
-wscript.echo (int(rnd*Max) + 1)
+wscript.echo (int(rnd*10) + 1)
